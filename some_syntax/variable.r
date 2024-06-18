5a84ac44b3db816a21c3648c5af4cf9dc5c4b7bc
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-06-18 10:29:12 +0700

    var

---
+++ some_syntax/variable.r
@@ -0,0 +1,31 @@
+# this comment i think
+
+
+# variable
+name <- "peli" # create variable
+manukCount <- 5L
+iqScore <- 70.5
+initial <- "p"
+isDone <- TRUE
+
+print(name)
+print(manukCount)
+print(iqScore)
+print(initial)
+
+print(typeof(name))
+print(typeof(manukCount))
+print(typeof(iqScore))
+print(typeof(initial))
+
+# concatenate
+print(paste(name, "gede"))
+print(paste(name, manukCount))
+
+
+# multiple variable
+coin <- score <- 0
+
+print(coin)
+print(score)
+
